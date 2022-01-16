2e5a4c0da52aacb5eb7aa8f474da89d7d3e1c753
Breon White <bdwhite92@gmail.com>
2022-01-16 16:36:40 -0500

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#main-content").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleToDo(document.querySelector('input').value);
+  })
 });
+
+function handleToDo(todo){
+  console.log(todo)
+  let li = document.createElement(`li`)
+  let btn = document.createElement(`button`)
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = `Remove Todo`
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.getElementById(`tasks`).appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
